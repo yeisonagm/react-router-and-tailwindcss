12576acb481198b6469e40488e2e5535b550b6d5
Yeison Garcia <garciamonsefu@gmail.com>
2023-07-29 15:55:09 -0500

    Responsive design for the buttons page

--- src/routes/Buttons.jsx
+++ src/routes/Buttons.jsx
@@ -5,21 +5,16 @@ export default function Buttons() {
         <>
             <Header />
             <div className='flex flex-col gap-8 bg-slate-100 py-16 rounded-xl shadow-lg mx-5 my-16'>
-                <div class="text-5xl font-extrabold ... mx-auto">
-                    <span class="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-500">
+                <div className="text-5xl font-extrabold ... mx-auto">
+                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-500">
                         Buttons
                     </span>
                 </div>
 
-                <div className='w-full flex flex-row justify-around'>
-                    <h1 className='font-bold text-2xl w-1/5 text-center'>Primary</h1>
-                    <h1 className='font-bold text-2xl w-1/5 text-center'>Ghost</h1>
-                    <h1 className='font-bold text-2xl w-1/5 text-center'>Link</h1>
-                </div>
-
                 {/* Buttons Primary */}
-                <div className='w-full flex flex-row'>
-                    <div className='w-1/3 flex flex-col gap-5 items-center'>
+                <div className='w-full flex flex-col items-center md:flex-row gap-10'>
+                    <div className='w-full md:w-1/3 flex flex-col gap-5 items-center'>
+                        <h1 className='font-bold text-2xl text-center mb-4'>Primary</h1>
                         <button className='bg-blue-500 px-5 py-2 rounded-xl hover:bg-blue-600 active:bg-blue-800'>
                             <span className='text-white font-semibold text-lg'>
                                 Button Title
@@ -40,7 +35,8 @@ export default function Buttons() {
                     </div>
 
                     {/*Buttons Ghost */}
-                    <div className='w-1/3 flex flex-col gap-5 items-center'>
+                    <div className='w-full md:w-1/3 flex flex-col gap-5 items-center'>
+                        <h1 className='font-bold text-2xl text-center mb-4'>Ghost</h1>
                         <button className='px-5 py-2 rounded-xl border-2 border-blue-400 hover:bg-blue-100 active:bg-blue-200'>
                             <span className='text-blue-500 font-semibold text-lg'>
                                 Button Title
@@ -61,7 +57,9 @@ export default function Buttons() {
                     </div>
 
                     {/* Buttons Link */}
-                    <div className='w-1/3 flex flex-col gap-5 items-center'>
+                    <div className='w-full md:w-1/3 flex flex-col gap-5 items-center'>
+                        <h1 className='font-bold text-2xl text-center mb-4'>Link</h1>
+
                         <button className='px-5 py-2 flex items-center gap-1'>
                             <span className='text-blue-400 font-semibold text-lg hover:text-blue-500 active:text-blue-700'>
                                 Button Title
