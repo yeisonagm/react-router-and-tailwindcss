10230bc15a3f6c42a5339694b42b584b205e84ae
Yeison Garcia <garciamonsefu@gmail.com>
2023-07-29 15:24:44 -0500

    Responsive design in the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
-        <nav className="bg-slate-700 text-white py-4 flex items-center px-8">
+        <nav className="bg-slate-700 text-white py-4 flex items-center px-4 sm:px-8">
             <ul className="grow">
                 <li>
                     <Link to={'/'} className="flex gap-3 items-center">
                         <img src="/design.png" alt="logo" />
-                        <span className="text-xl font-bold">
+                        <span className="hidden text-lg sm:  font-bold sm:block ">
                             Design styles
                         </span>
                     </Link>
                 </li>
             </ul>
 
-            <ul className="flex justify-center items-center gap-10 text-lg">
+            <ul className="flex justify-center items-center gap-5 md:gap-8 text-lg">
                 <li>
                     <Link to={'/'}
                         className="hover:border-b hover:text-gray-200">Home</Link>
